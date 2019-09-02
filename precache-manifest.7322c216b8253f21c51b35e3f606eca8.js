a7ca12b5d1080dd63dcc940ecb3a87d99497cbba
djinn09 <krunalh321@gmail.com>
2019-09-02 18:50:44 +0530

    Updates

---
+++ precache-manifest.7322c216b8253f21c51b35e3f606eca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369b47e2952fd62c99c663f3102371d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "98981075c8a0abe09206",
+    "url": "/monsters-rolodex/static/css/main.50306dc5.chunk.css"
+  },
+  {
+    "revision": "dfcff4a647f3d75cb0a5",
+    "url": "/monsters-rolodex/static/js/2.2bc53aab.chunk.js"
+  },
+  {
+    "revision": "98981075c8a0abe09206",
+    "url": "/monsters-rolodex/static/js/main.91ed6234.chunk.js"
+  },
+  {
+    "revision": "c9876256eac5b77b0b95",
+    "url": "/monsters-rolodex/static/js/runtime~main.f58a2f15.js"
+  }
+]);
